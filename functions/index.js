b286856b6c6ff38fd4755d9b4eba7d6a4a73d72d
AmilaWeerasinghe <amilaweerasinghe677@gmail.com>
2023-11-13 23:02:18 +0530

    object detection end point boiler plate

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,15 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const functions = require('firebase-functions');
+const cors = require('cors')({ origin: true });
+const axios = require('axios');
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
-
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.detectObjects = functions.https.onRequest((req, res) => {
+  cors(req, res, async () => {
+    try {
+      const response = await axios.post('end point', req.body);
+      res.json(response.data);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+});
